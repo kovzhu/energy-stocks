c7b2db190cc4e9040fd6aabcf965cc7457a43747
Kov <44253207+kovzhu@users.noreply.github.com>
2020-03-18 09:48:04 +0800

    Update Stcok prices for oil companies.py

--- Stcok prices for oil companies.py
+++ Stcok prices for oil companies.py
@@ -1,10 +1,14 @@
 import tushare as ts
 import pandas as pd
+import yfinance as yf
 import matplotlib.pyplot as plt
+from pandas_datareader import data as pdr
 
-token = 'your token for tushare'
+token = 'ff0abe11ff60a8ab965180971559c539d552c0e51de7e501593ff762'
 ts.set_token(token)
 pro = ts.pro_api()
+
+yf.pdr_override()
 
 OilCompanies = {'CNOOC': '00883.hk',
                 'Yanchang': '00346.hk',
@@ -12,8 +16,24 @@ OilCompanies = {'CNOOC': '00883.hk',
                 'Sinopec': '00386.hk'
                 }
 
-with pd.ExcelWriter('HK stocks.xlsx') as writer:
+IOCs = {
+    # 'Shell': 'RDS.A',
+    'BP': 'BP',
+    'Chevron': 'CVX',
+    'Total': 'TOT',
+    'Occidental': 'OXY',
+    'PetroChina': 'PTR',
+    'CNOOC': 'CEO',
+    'Devon': 'DVN',
+    'Chesapeake': 'CHK',
+}
+
+with pd.ExcelWriter('oil companies stocks.xlsx') as writer:
     for key in OilCompanies:
         df = pd.DataFrame(pro.hk_daily(ts_code=OilCompanies[key], start_date='20200101',
                                        end_date='20200317'))
         df.to_excel(writer, sheet_name=key)
+    for key in IOCs:
+        data = pd.DataFrame(pdr.get_data_yahoo(
+            IOCs[key], start="2020-01-01", end="2020-03-17"))
+        data.to_excel(writer, sheet_name=key)
